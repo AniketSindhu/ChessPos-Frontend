f58045f1f5b5f09df2549ff47b819878b8b1aab9
Aniket Sindhu <57598532+AniketSindhu@users.noreply.github.com>
2021-12-01 13:15:26 +0530

    chessboard alignment issues fixed + new pieces added

--- src/components/ChessGame.jsx
+++ src/components/ChessGame.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import { useRef, useState, useEffect } from "react";
 import Chess from "chess.js";
 import { Chessboard } from "react-chessboard";
+import wP from "../pieces/wP.png";
+import wN from "../pieces/wN.png";
+import wB from "../pieces/wB.png";
+import wK from "../pieces/wK.png";
+import wQ from "../pieces/wQ.png";
+import wR from "../pieces/wR.png";
+import bP from "../pieces/bP.png";
+import bN from "../pieces/bN.png";
+import bB from "../pieces/bB.png";
+import bK from "../pieces/bK.png";
+import bQ from "../pieces/bQ.png";
+import bR from "../pieces/bR.png";
 
 const socket = require("../connections/socket").socket;
 
@@ -13,6 +25,7 @@ function ChessGame({
   opponentAddress,
 }) {
   const [turn, setTurn] = useState(isCreator);
+
   useEffect(() => {
     socket.on("opponent move", (data) => {
       if (data.from !== yourAddress) {
@@ -23,6 +36,72 @@ function ChessGame({
       }
     });
   }, []);
+
+  const pieces = [
+    {
+      name: "wP",
+      peice: wP,
+    },
+    {
+      name: "wB",
+      peice: wB,
+    },
+    {
+      name: "wQ",
+      peice: wQ,
+    },
+    {
+      name: "wN",
+      peice: wN,
+    },
+    {
+      name: "wK",
+      peice: wK,
+    },
+    {
+      name: "wR",
+      peice: wR,
+    },
+    {
+      name: "bP",
+      peice: bP,
+    },
+    {
+      name: "bB",
+      peice: bB,
+    },
+    {
+      name: "bQ",
+      peice: bQ,
+    },
+    {
+      name: "bN",
+      peice: bN,
+    },
+    {
+      name: "bK",
+      peice: bK,
+    },
+    {
+      name: "bR",
+      peice: bR,
+    },
+  ];
+
+  const customPieces = () => {
+    const returnPieces = {};
+    pieces.map((p) => {
+      returnPieces[p.name] = ({ squareWidth }) => (
+        <img
+          style={{ width: squareWidth, height: squareWidth }}
+          src={p.peice}
+          alt={p.name}
+        />
+      );
+      return null;
+    });
+    return returnPieces;
+  };
 
   const chessboardRef = useRef();
   const [game, setGame] = useState(new Chess());
@@ -58,24 +137,36 @@ function ChessGame({
   return (
     <div>
       {yourAddress}
-      <Chessboard
-        animationDuration={200}
-        boardWidth={700}
-        position={game.fen()}
-        boardOrientation={isCreator ? "white" : "black"}
-        onPieceDrop={onDrop}
-        areArrowsAllowed={true}
-        arePiecesDraggable={turn}
-        customDarkSquareStyle={{ backgroundColor: "#A13245" }}
-        customLightSquareStyle={{ backgroundColor: "#EEB9B9" }}
-        customBoardStyle={{
-          borderRadius: "4px",
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          maxWidth: "800px",
           margin: "auto",
-          marginTop: "20px",
-          boxShadow: "0 5px 15px rgba(0, 0, 0, 0.5)",
+          padding: "30px",
         }}
-        ref={chessboardRef}
-      />
+      >
+        <Chessboard
+          id="StyledBoard"
+          animationDuration={200}
+          boardWidth={700}
+          position={game.fen()}
+          boardOrientation={isCreator ? "white" : "black"}
+          onPieceDrop={onDrop}
+          areArrowsAllowed={true}
+          arePiecesDraggable={turn}
+          customDarkSquareStyle={{ backgroundColor: "#A13245" }}
+          customLightSquareStyle={{ backgroundColor: "#EEB9B9" }}
+          customPieces={customPieces()}
+          customBoardStyle={{
+            borderRadius: "4px",
+            boxShadow: "0 5px 15px rgba(0, 0, 0, 0.5)",
+          }}
+          ref={chessboardRef}
+        />
+      </div>
+
       {opponentAddress}
     </div>
   );
