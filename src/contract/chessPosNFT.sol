9a3a9b11482e47e436b4f3e3a3c15388d5b77a1d
Rohit Duhan <rohitduhan2301@gmail.com>
2022-03-22 02:01:11 +0530

    Initial commit

--- src/contract/chessPosNFT.sol
+++ src/contract/chessPosNFT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
-contract NFT is ERC721URIStorage {
+contract NFT is ERC721URIStorage { 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
